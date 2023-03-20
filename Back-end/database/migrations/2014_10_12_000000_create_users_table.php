a018a6d82a48996278bc932c3e38020cd11ea00c
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-20 23:01:19 +0200

    JWT added

--- Back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ Back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('gender');
+            $table->date('dob');
+            $table->string('location');
             $table->rememberToken();
             $table->timestamps();
         });
