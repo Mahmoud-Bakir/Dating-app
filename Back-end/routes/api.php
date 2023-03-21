9721852d1a0720878e639c0168d0d0d07fc48398
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-21 23:42:54 +0200

    JWT authorization completed

--- Back-end/routes/api.php
+++ Back-end/routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registerController;
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout');
+    Route::post('/refresh', 'refresh');
 });
-Route::get('/register', [registerController::class, "register"]);
